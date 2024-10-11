856ec9bedc78029f5b5b8c17926dd28cd6a51be8
Sacsitha M. <UT01410@unicomtic.com>
2024-10-11 10:05:15 +0530

    initial commit

---
+++ FitnessProgramManagementSystem/FitnessProgram .cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem
+{
+    internal class FitnessProgram
+    {
+
+        private string fitnessProgramId { get; set; }
+        private string title { get; set; }
+        private string duration { get; set; }
+        private string price { get; set; }
+
+        public FitnessProgram(string FitnessProgramId, string Title, string Duration, string Price)
+        {
+            fitnessProgramId = FitnessProgramId;
+            title = Title;
+            duration = Duration;
+            price = Price;
+        }
+
+        public FitnessProgram()
+        {
+        }
+        public override string ToString()
+        {
+            return $"fitnessProgramId :{fitnessProgramId} \ntitle :{title}\nduration :{duration} \nprice :{price}";
+        }
+    }
+
+}
---
+++ FitnessProgramManagementSystem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var FitnessProgram1 = new FitnessProgram("FIT_001", "Beginners YOGA ", "3 months ", "10.00 ");
+            Console.WriteLine(FitnessProgram1);
+            Console.ReadLine();
+        }
+    }
+}
